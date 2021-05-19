0af58677979d0b0eef1db1223a15072ed2ad2f36
AbidaAli100 <abida.ali100@gmail.com>
2021-05-20 01:21:44 +0500

    added botton-on

---
+++ class 15/button-click.js
@@ -0,0 +1,10 @@
+// button code
+function easy(){
+    alert ('THIS IS LEVEL EASY');
+}
+function medium () {
+    alert ('THIS IS LEVEL MEDIUM');
+}
+function hard (){
+    alert ('THIS IS LEVEL HARD');
+}
